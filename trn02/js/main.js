9e4f764750eef723ca7fa0f994a919b996995ccf
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-05-10 14:04:11 +0900

    220510

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -3,6 +3,8 @@ $(function () {
     $('.top_banner i').on('click', function () {
         $('.top_banner').slideUp();
     });
+
+    // .main_slider 자식으로 figure들이 있어야 그걸 인식해서 slick-slider됨~!!
     $('.main_slider').slick({
         arrows: false,
         dots: true,
@@ -20,5 +22,31 @@ $(function () {
         // 아래처럼 설정해서 .on을 붙었다 뗐다 해줌
         current.addClass('on').siblings().removeClass('on');
     });
+
+    $('.movie .dec i:nth-of-type(1)').on('click', function () {
+        $('.movie video').trigger('play');
+    });
+    $('.movie .dec i:nth-of-type(2)').on('click', function () {
+        $('.movie video').trigger('pause');
+    });
+
+    $("#myMovie").YTPlayer({
+        videoURL: 'https://youtu.be/G7dUt84Fd_w',
+        containment: '.utube', 
+        autoPlay: true, 
+        mute: true, 
+        startAt: 0, 
+        opacity: 1, 
+        showControls: false,
+        playOnlyIfVisible: true,
+    });
+    $('.utube i:nth-of-type(1)').on('click', function () {
+        // YTPPlay 플러그인에서 정의한 명령어 YTPPlay(),YTPPause()..
+        $('#myMovie').YTPPlay();
+    });
+    $('.utube i:nth-of-type(2)').on('click', function () {
+        $('#myMovie').YTPPause();
+    });
+
     ////////////////////////////////////////////////
 })
