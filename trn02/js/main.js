ba0d12f529c8471bcd5dffd44c35143a766e4e4c
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-05-11 17:25:21 +0900

    Graff 홈피 pc버전 완성

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -4,6 +4,7 @@ $(function () {
         $('.top_banner').slideUp();
     });
 
+    // 슬릭슬라이더 설정 앞에 fuction넣기
     $('.main_slider').on('init reInit afterChange', function () {
         // 슬릭슬라이더에서 정의한 init reInit afterChange이벤트
         console.log('변했네??');
@@ -14,7 +15,6 @@ $(function () {
         // 아래처럼 설정해서 .on을 붙었다 뗐다 해줌
         current.addClass('on').siblings().removeClass('on');
     });
-
     // .main_slider 자식으로 figure들이 있어야 그걸 인식해서 slick-slider됨~!!
     $('.main_slider').slick({
         arrows: false,
@@ -24,6 +24,7 @@ $(function () {
         pauseOnFocus: false,
     });
 
+    // video 태그
     $('.movie .dec i:nth-of-type(1)').on('click', function () {
         $('.movie video').trigger('play');
     });
@@ -31,6 +32,14 @@ $(function () {
         $('.movie video').trigger('pause');
     });
 
+    // ytplayer plugin
+    $('.utube i:nth-of-type(1)').on('click', function () {
+        // YTPPlay 플러그인에서 정의한 명령어 YTPPlay(),YTPPause()..
+        $('#myMovie').YTPPlay();
+    });
+    $('.utube i:nth-of-type(2)').on('click', function () {
+        $('#myMovie').YTPPause();
+    });
     $("#myMovie").YTPlayer({
         videoURL: 'https://youtu.be/G7dUt84Fd_w',
         containment: '.utube',
@@ -41,13 +50,44 @@ $(function () {
         showControls: false,
         playOnlyIfVisible: true,
     });
-    $('.utube i:nth-of-type(1)').on('click', function () {
-        // YTPPlay 플러그인에서 정의한 명령어 YTPPlay(),YTPPause()..
-        $('#myMovie').YTPPlay();
+
+    // 제품슬라이더
+    // 슬릭슬라이더 설정 앞에 fuction넣기
+    $('.product_slider').on('init reInit afterChange', function () {
+        console.log('또변했네??');
+        // centermode 활성화된 슬라이드에 .slick-current라는 class가 붙는데 그걸 그냥 center라고 정의해줌
+        let center = $('.product_slider .slick-current');
+        console.log(center);
+        center.addClass('on').siblings().removeClass('on');
     });
-    $('.utube i:nth-of-type(2)').on('click', function () {
-        $('#myMovie').YTPPause();
+    $(".product_list i.s_left").on("click", function () {
+        $(".product_slider").slick("slickPrev");
+    });
+    $(".product_list i.s_right").on("click", function () {
+        $(".product_slider").slick("slickNext");
+    });
+    $('.product_slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        autoplay: true,
+        pauseOnHover: false,
     });
 
+    // main의 tab
+        $('.tab_link>li').on('click', function () {
+        var idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.tab_content>li').eq(idx).addClass('on').siblings().removeClass('on');
+    })
+
+    // footer의 family link
+    $('.footer #link').on('change', function () {
+        var lik = $(this).val();
+        console.log(lik);
+        if (lik) { window.open(lik) };
+    })
     ////////////////////////////////////////////////
 })
