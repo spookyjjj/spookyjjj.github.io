1be71bb04812a9795fcf541ece24789246ce0858
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-08-23 01:17:23 +0900

    Update Main3.java

--- ee/220822/src/Main3.java
+++ ee/220822/src/Main3.java
@@ -11,22 +11,24 @@ import java.net.URLEncoder;
 import com.example.AppleSearchResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class Main3 {
+public class Main2 {
 	public static void main(String[] args) throws UnsupportedEncodingException, MalformedURLException {
 		String apiURL = "https://itunes.apple.com/search?term=";
-
 		String requestURL = apiURL + URLEncoder.encode("아이유", "UTF-8");
-
 		URL url = new URL(requestURL);
+		
 		HttpURLConnection conn = null;
-
 		try {
 			conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 
 			int responseCode = conn.getResponseCode();
+			//응답코드가 정상일때(HTTP_OK:200)만 진행하고, 그 이외일때는 에러난거니깐 진행 ㄴㄴ
 			if (responseCode == HttpURLConnection.HTTP_OK) {
 				String respBody = readBody(conn.getInputStream());
+				// 보낸 요청에 대한 응답이 body에 담겨있으므로 응답 body 살펴보기
+				System.out.println(respBody);
+				//이걸로 jsonschema2pojo 돌려서 AppleSearchResult객체 생성하기
 				
 				ObjectMapper mapper = new ObjectMapper();
 				AppleSearchResult apple = mapper.readValue(respBody, AppleSearchResult.class);
