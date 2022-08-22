bcc0f7db14b91103f9a82e259f9708c6ebae0875
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-08-23 00:08:21 +0900

    Update Main.java

--- ee/220822/src/Main.java
+++ ee/220822/src/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		ObjectMapper mapper = new ObjectMapper();
 		ObjectNode node = mapper.createObjectNode();
 		
+		//노드에 값넣기 : put/set 모두 가능
 		node.put("name", "홍길동");
 		node.put("age", 22);
 		node.put("boolean", true);
@@ -18,9 +19,13 @@ public class Main {
 		node2.put("이름", "둘리");
 		node2.put("나이", 23);
 		
+// 		node.put("bf", node2);
 		node.set("bf", node2);
+		//객체 자체도 node안에 넣을 수 있다~!
+		//원래는 put으로 넣었는데 diprecated떠서(업뎃되면 사라질 기능이라고 알려주는거) set으로 넣어줬음
 		
 		String json = mapper.writeValueAsString(node);
 		System.out.println(json);
+		//결과 : {"name":"홍길동","age":22,"boolean":true,"bf":{"이름":"둘리","나이":23}}
 	}
 }
