8b2d1216a944015233141443d1b3d1fdcef7a315
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-08-23 21:34:16 +0900

    Update Main.java

--- ee/220822/src/Main.java
+++ ee/220822/src/Main.java
@@ -4,12 +4,11 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Main {
 	public static void main(String[] args) throws JsonProcessingException {
-		// 사람
-		// 이름 = 홍길동
-		// 나이 = 22
-		ObjectMapper mapper = new ObjectMapper();
+		//jackson에서는 ObjectMapper로 모든걸 시작!
+		ObjectMapper mapper = new ObjectMapper(); 
 		ObjectNode node = mapper.createObjectNode();
 		
+		//노드에 값넣기 : put/set 모두 가능
 		node.put("name", "홍길동");
 		node.put("age", 22);
 		node.put("boolean", true);
@@ -18,9 +17,14 @@ public class Main {
 		node2.put("이름", "둘리");
 		node2.put("나이", 23);
 		
+// 		node.put("bf", node2);
 		node.set("bf", node2);
+		//객체 자체도 node안에 넣을 수 있다~!
+		//원래는 put으로 넣었는데 diprecated떠서(업뎃되면 사라질 기능이라고 알려주는거) set으로 넣어줬음
 		
 		String json = mapper.writeValueAsString(node);
 		System.out.println(json);
+		//{"name":"홍길동","age":22,"boolean":true,"bf":{"이름":"둘리","나이":23}}
+		//어디서는 key값이 'name'이고 어디서는 '이름'이면 데이터활용이 어려움 -> 일정한규칙대로 만들기(객체로 )
 	}
 }
