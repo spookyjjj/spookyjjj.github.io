7003bb4fcef4a0983dd14f1ed17ba9e31ef6ac90
spookyjjj <104603810+spookyjjj@users.noreply.github.com>
2022-08-23 23:01:19 +0900

    Update Main5.java

--- ee/220822/src/Main5.java
+++ ee/220822/src/Main5.java
@@ -7,13 +7,17 @@ import okhttp3.Response;
 public class Main5 {
 	public static void main(String[] args) {
 		String apiURL = "http://httpbin.org/get";
-		
+		//OkHttp로 api사용해보기~
+		//=============get방식
 		OkHttpClient client = new OkHttpClient();
 		Request req = new Request.Builder().url(apiURL).build();
+		//Request는 Builder()로 설정시작. append로 붙여가면서 설정. 끝나면 build()로 마무리
 		
 		try (Response resp = client.newCall(req).execute()) {
+		//Response는 OkHttpClient에서 콜 객체를 생성해서 실행한 후 받아옴
 			if (resp.code() == 200) {
 				System.out.println(resp.body().string());
+				//이렇게 바로 읽어낼 수 있다~ 너무 편하다~
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
